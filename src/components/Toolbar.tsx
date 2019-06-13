a7b5bbf503b93d421b6d50c80c4b1782616e7577
Alexander Wilde <alexander@salesseek.co.uk>
2019-06-13 15:59:10 +0100

    Split toolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -65,73 +65,76 @@ export class Toolbar extends React.Component<ToolbarProps, ToolbarState> {
 
   render() {
     return (
-      <div className='toolbar'>
-        <div><input type='color' value={this.state.color} onChange={this.onColorChange}/></div>
-        
-        <div className='material-icons md-light' style={{cursor: 'pointer'}} onClick={this.onPrintClick}>print</div>
-        <div className='material-icons md-light' style={{cursor: 'pointer'}} onClick={this.onMoveClick}>zoom_out_map</div>
-        <div className='material-icons md-light' draggable onDragStart={this.onDragStart} onClick={this.onTextClick}>edit</div>
+      <div className='toolbar-container'>
+        <div className='toolbar' style={{minWidth: 'fit-content'}}>
+          <div><input type='color' value={this.state.color} onChange={this.onColorChange}/></div>
+          
+          <div className='material-icons md-light' style={{cursor: 'pointer'}} onClick={this.onPrintClick}>print</div>
+          <div className='material-icons md-light' style={{cursor: 'pointer'}} onClick={this.onMoveClick}>zoom_out_map</div>
+          <div className='material-icons md-light' draggable onDragStart={this.onDragStart} onClick={this.onTextClick}>edit</div>
+        </div>
+        <div className='toolbar'>
+          <ToolbarIcon action='hex-enemy-vertical' />
+          <ToolbarIcon action='hex-enemy-horizontal' />
+          
+          <ToolbarIcon action='attack' />
+          <ToolbarIcon action='heal' />
+          <ToolbarIcon action='range' />
+          <ToolbarIcon action='target' />
+          <ToolbarIcon action='move' />
+          <ToolbarIcon action='jump' />
+          <ToolbarIcon action='flying' />
+          <ToolbarIcon action='shield' />
+          <ToolbarIcon action='retaliate' />
+          <ToolbarIcon action='loot' />
+          <ToolbarIcon action='xp' data={{
+            nodes: [{
+              object: 'text',
+              text: '1',
+            }],
+          }} />
+          <ToolbarIcon action='round' />
+          <ToolbarIcon action='persistent' />
 
-        <ToolbarIcon action='hex-enemy-vertical' />
-        <ToolbarIcon action='hex-enemy-horizontal' />
-        
-        <ToolbarIcon action='attack' />
-        <ToolbarIcon action='heal' />
-        <ToolbarIcon action='range' />
-        <ToolbarIcon action='target' />
-        <ToolbarIcon action='move' />
-        <ToolbarIcon action='jump' />
-        <ToolbarIcon action='flying' />
-        <ToolbarIcon action='shield' />
-        <ToolbarIcon action='retaliate' />
-        <ToolbarIcon action='loot' />
-        <ToolbarIcon action='xp' data={{
-          nodes: [{
-            object: 'text',
-            text: '1',
-          }],
-        }} />
-        <ToolbarIcon action='round' />
-        <ToolbarIcon action='persistent' />
+          <ToolbarIcon action='element-all' />
+          <ToolbarIcon action='element-fire' />
+          <ToolbarIcon action='element-ice' />
+          <ToolbarIcon action='element-air' />
+          <ToolbarIcon action='element-earth' />
+          <ToolbarIcon action='element-light' />
+          <ToolbarIcon action='element-dark' />
 
-        <ToolbarIcon action='element-all' />
-        <ToolbarIcon action='element-fire' />
-        <ToolbarIcon action='element-ice' />
-        <ToolbarIcon action='element-air' />
-        <ToolbarIcon action='element-earth' />
-        <ToolbarIcon action='element-light' />
-        <ToolbarIcon action='element-dark' />
+          <ToolbarIcon action='effect-add-target' />
+          <ToolbarIcon action='effect-push' />
+          <ToolbarIcon action='effect-pull' />
+          <ToolbarIcon action='effect-pierce' />
 
-        <ToolbarIcon action='effect-add-target' />
-        <ToolbarIcon action='effect-push' />
-        <ToolbarIcon action='effect-pull' />
-        <ToolbarIcon action='effect-pierce' />
+          <ToolbarIcon action='status-poison' />
+          <ToolbarIcon action='status-wound' />
+          <ToolbarIcon action='status-immobilize' />
+          <ToolbarIcon action='status-disarm' />
+          <ToolbarIcon action='status-stun' />
+          <ToolbarIcon action='status-muddle' />
+          <ToolbarIcon action='curse' />
 
-        <ToolbarIcon action='status-poison' />
-        <ToolbarIcon action='status-wound' />
-        <ToolbarIcon action='status-immobilize' />
-        <ToolbarIcon action='status-disarm' />
-        <ToolbarIcon action='status-stun' />
-        <ToolbarIcon action='status-muddle' />
-        <ToolbarIcon action='curse' />
+          <ToolbarIcon action='status-invisible' />
+          <ToolbarIcon action='status-strengthen' />
+          <ToolbarIcon action='bless' />
 
-        <ToolbarIcon action='status-invisible' />
-        <ToolbarIcon action='status-strengthen' />
-        <ToolbarIcon action='bless' />
+          <ToolbarIcon action='card-lost' />
+          <ToolbarIcon action='card-recover' />
+          <ToolbarIcon action='card-unrecoverable' />
 
-        <ToolbarIcon action='card-lost' />
-        <ToolbarIcon action='card-recover' />
-        <ToolbarIcon action='card-unrecoverable' />
+          <ToolbarIcon action='item-head' />
+          <ToolbarIcon action='item-body' />
+          <ToolbarIcon action='item-feet' />
+          <ToolbarIcon action='item-onehand' />
+          <ToolbarIcon action='item-twohands' />
+          <ToolbarIcon action='item-small' />
 
-        <ToolbarIcon action='item-head' />
-        <ToolbarIcon action='item-body' />
-        <ToolbarIcon action='item-feet' />
-        <ToolbarIcon action='item-onehand' />
-        <ToolbarIcon action='item-twohands' />
-        <ToolbarIcon action='item-small' />
-
-        <ToolbarIcon action='item-spent' />
-        <ToolbarIcon action='item-refresh' />
+          <ToolbarIcon action='item-spent' />
+          <ToolbarIcon action='item-refresh' />
+        </div>
       </div>
     )
   }
